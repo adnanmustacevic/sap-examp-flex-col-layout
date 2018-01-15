5202a1d6bddaf425c3e75f74011d77233d96cfe3
adnanmustacevic <adnan.mustaa@kadaster.nl>
2018-01-15 16:49:10 +0000

    Create project

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("nl.kadaster.test.flexcollayout.mycolleagues.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("nl.kadaster.test.flexcollayout.mycolleagues.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});

3fdf4cf966099bc0da57aaa2e342559522637bf0
Koopman K. <karst.koopman@kadaster.nl>
2018-02-08 10:02:02 +0000

    Resourcebundle repaired

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -13,17 +13,17 @@ sap.ui.define([
 
 		onLinkClick: function(oEvent) {
 			var that = this;
-			//var resourceBundle = sap.ui.getCore().byId("app").getModel("i18n").getResourceBundle();			
+			var resourceBundle = this.getView().getModel("i18n").getResourceBundle();	
 			var bindingContext = oEvent.getSource().getBindingContext();
 			var persNo = bindingContext.getModel().getObject(bindingContext.getPath()).profileId;
 			
 		    jQuery.when(ODataServiceUtil.getEmployeeData(persNo)).then(function(persInfo) {
 				persInfo.photoURL = "/sap/opu/odata/sap/ZHCM_EMPLOYEE_LOOKUP_SRV/EmployeeInfoSet('" + persNo + "')/$value";
-				// persInfo.contactgegevens = resourceBundle.getText("CONTACT_DATA").trim();
-				// persInfo.doorkiesnummer = resourceBundle.getText("DOORKIESNUMMER_PERGEG_DISP").trim();
-				// persInfo.mobielnummer = resourceBundle.getText("MOBIEL_PERGEG_DISP").trim();
-				// persInfo.email = resourceBundle.getText("EMAIL_PERGEG_DISP").trim();
-				// persInfo.kamernummer = resourceBundle.getText("KAMERNUMMER_ORGGEG_DISP").trim();
+				persInfo.contactgegevens = resourceBundle.getText("CONTACT_DATA").trim();
+				persInfo.doorkiesnummer = resourceBundle.getText("DOORKIESNUMMER_PERGEG_DISP").trim();
+				persInfo.mobielnummer = resourceBundle.getText("MOBIEL_PERGEG_DISP").trim();
+				persInfo.email = resourceBundle.getText("EMAIL_PERGEG_DISP").trim();
+				persInfo.kamernummer = resourceBundle.getText("KAMERNUMMER_ORGGEG_DISP").trim();
 				persInfo.Mobiel = persInfo.Mobiel ? Util.formatMobielNummer(persInfo.Mobiel, 2) : "";
 				persInfo.Email = persInfo.Email ? persInfo.Email.toLowerCase() : "";
 				that.bus.publish("flexible", "setDetailDetailPage");				
